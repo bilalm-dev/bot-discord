72d8a342f42515e865cccb5150c6d8dc7186a72d
billly117 <bilalmesbahi@free.fr>
2026-05-09 22:15:34 +0200

    jour1: setup bot Discord, connextion et intents

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ {bot.user} est connecté et prêt !")
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ {len(synced)} commande(s) slash synchronisée(s)")
+    except Exception as e:
+        print(f"❌ Erreur de synchronisation : {e}")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
